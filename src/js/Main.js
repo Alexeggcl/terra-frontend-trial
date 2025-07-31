3e839f69378f5f4e629d9555eab94081ebaf3b66
Alexeggcl <alejandroeggcl@gmail.com>
2025-07-31 16:27:45 +0200

    fix: Accessibility Implementation (Task 7)

--- src/js/Main.js
+++ src/js/Main.js
@@ -10,33 +10,38 @@ class Main{
         }
         this.instances = []
         this.init();
-        this.events()
+        this.events();
     }
 
     init(){
-
         this.instances['collapsify'] = [];
         this.instances['collapsify'][0]= new Collapsify({});
-
 
         this.instances['RevealInstances'] = [];
         this.DOM.revealItems.forEach((item, index) => {
             this.instances['RevealInstances'][index] = new RevealItem({ element: item });
         });
-
     }
 
     events(){
         if (this.DOM.playBtn) {
             this.DOM.playBtn.addEventListener("click", () => {
                 const lottieName = this.DOM.playBtn.dataset.lottie;
-                window.WL[lottieName].play();
+                if (window.WL && window.WL[lottieName]) {
+                    window.WL[lottieName].play();
+                    this.DOM.playBtn.setAttribute('aria-pressed', 'true');
+                    this.DOM.pauseBtn.setAttribute('aria-pressed', 'false');
+                }
             });
         }
         if (this.DOM.pauseBtn) {
             this.DOM.pauseBtn.addEventListener("click", () => {
-                const lottieName = this.DOM.playBtn.dataset.lottie;
-                window.WL[lottieName].pause();
+                const lottieName = this.DOM.pauseBtn.dataset.lottie;
+                if (window.WL && window.WL[lottieName]) {
+                    window.WL[lottieName].pause();
+                    this.DOM.pauseBtn.setAttribute('aria-pressed', 'true');
+                    this.DOM.playBtn.setAttribute('aria-pressed', 'false');
+                }
             });
         }
     }
